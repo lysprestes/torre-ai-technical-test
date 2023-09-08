eb9a892876f731433deaf08eb1e800283e299e09
Lys Prestes <lysprestes@gmail.com>
2023-09-07 20:22:20 -0400

    Fixing folder structure

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import Search from './components/Search';
+
+function App() {
+  return (
+    <div className="App">
+      Hello World!
+    </div>
+  );
+}
+
+export default App;
