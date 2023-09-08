7f058e84dd075a2c2788186b24595660a303516f
Lys Prestes <lysprestes@gmail.com>
2023-09-07 20:20:02 -0400

    Setting up proxy server

---
+++ torre-api-proxy/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.get('/api/bios/:username', async (req, res) => {
+  try {
+    const username = req.params.username;
+    const apiUrl = `https://torre.bio/api/bios/${username}`;
+
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while fetching data from Torre API' });
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('Server is up!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Proxy server is running on port ${PORT}`);
+});
